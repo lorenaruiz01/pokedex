18ad5015209384bcc70512cde3155fecbff98e53
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-08-08 14:04:00 -0700

    use fetch(url) to request info and convert to json

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const fetchPokemon = () => {
     const url = `https://pokeapi.co/api/v2/pokemon/?limit=151`;
+    fetch(url)                                      // fetch the url, which then returns a promise. in that promise, define the callback which is the response. the response is an arrow function to call the response with a json method which then converts the response into json to get the body of the response. 
+        .then( response => {
+            return response.json(); 
+        })
+          
 }
 
 fetchPokemon();
