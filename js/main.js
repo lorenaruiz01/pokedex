2e5b9dc8c90d66b0403932951ccb9bd10242d561
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-08-08 16:53:10 -0700

    remove duplicate code and log pokemon object

--- js/main.js
+++ js/main.js
@@ -12,11 +12,6 @@ const fetchPokemon = () => {
                 image: data.sprites['front_default'],
                 types: data.types.map((type) => type.type.name).join(', ')      // use map() method to iterate over each types element to create new array of types then join() those type names into a string separate by commas
             };
-            pokemon['name'] = data.name;
-            pokemon['id'] = data.id;
-            pokemon['image'] = data.sprites['front_default'];
-            pokemon['types'] = data.types.map((type) => type.type.name).join(', ');     
-            
             console.log(pokemon);
         });       
 };
