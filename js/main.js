c9547be98d75674d711fe572f18bbcb08c94dc43
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-08-08 14:30:52 -0700

    test with first object in api

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const fetchPokemon = () => {
-    const url = `https://pokeapi.co/api/v2/pokemon/?limit=151`;
+    const url = `https://pokeapi.co/api/v2/pokemon/1`;
     fetch(url)                                      // fetch the url, which then returns a promise. in that promise, define the callback which is the response. the response is an arrow function to call the response with a json method which then converts the response into json to get the body of the response. 
         .then( response => {
             return response.json(); 
