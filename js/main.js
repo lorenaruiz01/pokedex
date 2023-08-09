3095a5ff9101d85a00ffc44c836734a254c2b164
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-08-08 20:45:27 -0700

    create promises array with pokemon objects

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
+
 const fetchPokemon = () => {
-    const url = `https://pokeapi.co/api/v2/pokemon/1`;
-    fetch(url)                                      // fetch the url, which then returns a promise. in that promise, define the callback which is the response. the response is an arrow function to call the response with a json method which then converts the response into json to get the body of the response. 
-        .then( response => {
-            return response.json(); 
-        })
-        .then( data => {                                // response.json returns another promise so then we call .then again to get the data and console.log the data
-            console.log(data);
-            const pokemon = {
-                name: data.name,
-                id: data.id,
-                image: data.sprites['front_default'],
-                types: data.types.map((type) => type.type.name).join(', ')      // use map() method to iterate over each types element to create new array of types then join() those type names into a string separate by commas
-            };
-            console.log(pokemon);
-        });       
+
+    const promises = [];
+    for (let i = 1; i <= 150; i++ ) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then( (response) => response.json()));                                      // fetch the url, which then returns a promise. in that promise, define the callback which is the response. the response is an arrow function to call the response with a json method which then converts the response into json to get the body of the response. 
+    }
+
+    Promise.all(promises).then( (results) => {
+        const pokemon = results.map( (data) => ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            types: data.types.map((type) => type.type.name).join(', ')
+        }));
+        console.log(pokemon)
+    });
 };
+
+const displayPokemon = (pokemon) => {
+
+}
 
 fetchPokemon();
