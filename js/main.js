4641790b3a890123475153c935702765ee84ea5a
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-08-10 22:51:57 -0700

    change variable name

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ const displayPokemon = (pokemon) => {
 const selectPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const response = await fetch(url);
-    const data = await response.json();
+    const individualPokemonData = await response.json();
 
 }
 
