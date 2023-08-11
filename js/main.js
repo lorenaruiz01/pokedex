ab57e9cbfc2a4109a71cc2f634db5ec90d024f53
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-08-10 23:27:24 -0700

    remove code

--- js/main.js
+++ js/main.js
@@ -49,25 +49,3 @@ const displayPopup = (pokemon) => {
 };
 
 fetchPokemon();
-
-
-
-//////// scratch code: 
-
-// const promises = [];
-//     for (let i = 1; i <= 150; i++ ) {
-//         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-//         promises.push(fetch(url).then( (response) => response.json()));                                      // fetch the url, which then returns a promise. in that promise, define the callback which is the response. the response is an arrow function to call the response with a json method which then converts the response into json to get the body of the response. 
-//     }
-
-//     Promise.all(promises).then( (results) => {
-//         const pokemon = results.map( (data) => ({
-//             name: data.name,
-//             id: data.id,
-//             image: data.sprites['front_default'],
-//             height: data.height,
-//             types: data.types.map((type) => type.type.name).join(', ')
-//         }));
-//         displayPokemon(pokemon);
-//     });
-
