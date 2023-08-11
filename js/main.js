1d2a5e36cd423eb082b5ce5e7dcc9de073e41b2e
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-08-10 19:06:21 -0700

    remove fetchPokemon code

--- js/main.js
+++ js/main.js
@@ -3,22 +3,7 @@ const pokedex = document.getElementById('pokedex');
 const fetchPokemon = () => {
     const url = `https://pokeapi.co/api/v2/pokemon/?limit=150`;
 
-    const promises = [];
-    for (let i = 1; i <= 150; i++ ) {
-        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        promises.push(fetch(url).then( (response) => response.json()));                                      // fetch the url, which then returns a promise. in that promise, define the callback which is the response. the response is an arrow function to call the response with a json method which then converts the response into json to get the body of the response. 
-    }
-
-    Promise.all(promises).then( (results) => {
-        const pokemon = results.map( (data) => ({
-            name: data.name,
-            id: data.id,
-            image: data.sprites['front_default'],
-            height: data.height,
-            types: data.types.map((type) => type.type.name).join(', ')
-        }));
-        displayPokemon(pokemon);
-    });
+    
 };
 
 const displayPokemon = (pokemon) => {
@@ -35,3 +20,24 @@ const displayPokemon = (pokemon) => {
 };
 
 fetchPokemon();
+
+
+
+//////// scratch code: 
+
+// const promises = [];
+//     for (let i = 1; i <= 150; i++ ) {
+//         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+//         promises.push(fetch(url).then( (response) => response.json()));                                      // fetch the url, which then returns a promise. in that promise, define the callback which is the response. the response is an arrow function to call the response with a json method which then converts the response into json to get the body of the response. 
+//     }
+
+//     Promise.all(promises).then( (results) => {
+//         const pokemon = results.map( (data) => ({
+//             name: data.name,
+//             id: data.id,
+//             image: data.sprites['front_default'],
+//             height: data.height,
+//             types: data.types.map((type) => type.type.name).join(', ')
+//         }));
+//         displayPokemon(pokemon);
+//     });
